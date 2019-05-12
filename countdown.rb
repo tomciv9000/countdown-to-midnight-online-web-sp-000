2ec800e67b686f39f42d46b0977c66efd5908fa7
Tom White <eugene.white@gmail.com>
2019-05-12 15:30:00 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,8 +4,15 @@ def countdown(number)
   while number > 0 
     puts "#{number} SECOND(S)!"
     number -= 1 
-    sleep 1 
   end
   return "HAPPY NEW YEAR!"
 end
 
+def countdown_with_sleeo(int)
+  while number > 0 
+    puts "#{int} SECOND(S)!"
+    sleep 1 
+    number -= 1 
+  end
+  return "HAPPY NEW YEAR!"
+end
